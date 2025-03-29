c8bfa223d3c266793a45d3752468bf4439ae774c
shahrukh <shahrukhbutt804@gmail.com>
2025-03-29 16:00:44 +0500

    add wow.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import StoreProvider from "@/lib/redux/store/provider";
+import WowJsProvider from "@/utils/WowJsProvider";
+import "animate.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -29,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <StoreProvider>
-        {children}
+          <WowJsProvider>{children}</WowJsProvider>
         </StoreProvider>
       </body>
     </html>
